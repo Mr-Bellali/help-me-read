c96cde36e4bbab7a907ad7254aac4ebbb20533d8
Yassine Bellali <bellali.yassine52@gmail.com>
2025-08-09 15:00:39 +0100

    setup redis and celery to work with the api

---
+++ api/celery_worker.py
@@ -0,0 +1,17 @@
+# Followed the tutorialin medium: https://medium.com/@hitorunajp/celery-and-background-tasks-aebb234cae5d
+
+from celery import Celery
+import time
+
+# Configure Celery to use Redis as the message broker
+celery = Celery(
+    "worker", # This is the name of your Celery applicarion
+    broker="redis://localhost:6379/0", # This is the Redis connection string
+    backend="redis://localhost:6379/0", # Optional, for storing task results
+)
+
+@celery.task
+def write_log_celery(message: str):
+    time.sleep(30)
+    with open("log_celery.txt", "a") as f:
+        f.write(f"{message}/n")
--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from routes import analyse
+from celery_worker import write_log_celery
 app = FastAPI(
     title="API for help me read project"
 )
@@ -12,3 +13,9 @@ async def root():
     Root endpoint.
     """
     return {"message": "Hello from api"}
+
+# Play with celery to practice
+@app.post("/notify")
+async def notify_user(email: str):
+    write_log_celery.delay(f"Notification sent to {email}")
+    return {"message": f"Email will be sent to {email}"}
