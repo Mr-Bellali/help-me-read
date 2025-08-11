1934188f08a2af536b99bb89ad20db3727729a13
Yassine Bellali <bellali.yassine52@gmail.com>
2025-08-11 11:13:21 +0100

    update requirements and enhance document analysis logic with LangChain integration

--- api/routes/analyse.py
+++ api/routes/analyse.py
@@ -4,6 +4,11 @@ Analyse API endpoints for analysing the documents
 
 from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
 
 router = APIRouter()
 
@@ -26,8 +31,48 @@ async def analyse_document(
         raise HTTPException(status_code=400, detail="the document content must be provided for the analysis")
 
     # TODO: Implement actual analysis logic here
+    load_dotenv()
+
+    class ResearchResponse(BaseModel):
+        topic:str
+        summary: str
+        sources: list[str]
+        tools_used: list[str]
+
+    llm = ChatOpenAI(model="gpt-4o-mini")
+    parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                """
+                You are a research assistant thet will help generate a research paper.
+                Answer the user query and use neccessary tools.
+                Wrap the output in this format and provide no other text \n{format_instructions}
+                """,
+            ),
+            ("placeholder", "{chat_history}"),
+            ("human", "{query}"),
+            ("placeholder", "{agent_scratchpad}"),
+            
+        ]
+    ).partial(format_instructions=parser.get_format_instructions())
+
+    agent = create_tool_calling_agent(
+        llm=llm,
+        prompt=prompt,
+        tools=[]
+    )
+
+    agent_executor = AgentExecutor(agent=agent, tools=[], verbose=True)
+
+    raw_response = agent_executor.invoke({"query": "What is the capital of France?"})
+
+    structured_response = parser.parse(raw_response.get("output"))
+
     return {
         "status": "success",
-        "message": "Document analysis started",
+        "message": structured_response,
         "document_length": len(request.document_content)
     }
